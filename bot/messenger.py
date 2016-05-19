ffe2c78ae80ae5b55f48a4edf08783397a93face
idontbyte <jsgilbert@live.com>
2016-05-19 12:53:12 +0100

    clean up and add api method

--- bot/messenger.py
+++ bot/messenger.py
@@ -1,9 +1,7 @@
 import logging
 import random
 
-
 logger = logging.getLogger(__name__)
-
 
 class Messenger(object):
     def __init__(self, slack_clients):
@@ -20,54 +18,22 @@ class Messenger(object):
     def write_help_message(self, channel_id):
         bot_uid = self.clients.bot_user_id()
         txt = '{}\n{}\n{}\n{}'.format(
-            "I'm your friendly Slack bot written in Python.  I'll *_respond_* to the following commands:",
-            "> `hi <@" + bot_uid + ">` - I'll respond with a randomized greeting mentioning your user. :wave:",
-            "> `<@" + bot_uid + "> joke` - I'll tell you one of my finest jokes, with a typing pause for effect. :laughing:",
-            "> `<@" + bot_uid + "> attachment` - I'll demo a post with an attachment using the Web API. :paperclip:",
+            "I am pollbot - tell me what to do:",
+            "> `<@" + bot_uid + "> setapi` - sets the API url for posting collected data",
             "> `<@" + bot_uid + "> getbugs` - I'll message everyone in channel to see if they have any bugs to report this week")
         self.send_message(channel_id, txt)
 
-    def write_greeting(self, channel_id, user_id):
-        greetings = ['Hi', 'Hello', 'Nice to meet you', 'Howdy', 'Salutations']
-        txt = '{}, <@{}>!'.format(random.choice(greetings), user_id)
-        self.send_message(channel_id, txt)
-
-    def write_prompt(self, channel_id):
-        bot_uid = self.clients.bot_user_id()
-        txt = "I'm sorry, I didn't quite understand... Can I help you? (e.g. `<@" + bot_uid + "> help`)"
-        self.send_message(channel_id, txt)
-
-    def write_joke(self, channel_id):
-        question = "Why did the python cross the road?"
-        self.send_message(channel_id, question)
-        self.clients.send_user_typing_pause(channel_id)
-        answer = "To eat the chicken on the other side! :laughing:"
-        self.send_message(channel_id, answer)
-
-
-    def write_error(self, channel_id, err_msg):
-        txt = ":face_with_head_bandage: my maker didn't handle this error very well:\n>```{}```".format(err_msg)
-        self.send_message(channel_id, txt)
-
-    def demo_attachment(self, channel_id):
-        txt = "Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots."
-        attachment = {
-            "pretext": "We bring bots to life. :sunglasses: :thumbsup:",
-            "title": "Host, deploy and share your bot in seconds.",
-            "title_link": "https://beepboophq.com/",
-            "text": txt,
-            "fallback": txt,
-            "image_url": "https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png",
-            "color": "#7CD197",
-        }
-        self.clients.web.chat.post_message(channel_id, txt, attachments=[attachment], as_user='true')
-        
     def get_bugs(self, channel_id):
         txt = "Please can you tell me - are there any open bugs to report this week?"
         response = self.clients.web.channels.info(channel_id)
         for user in response.body['channel']['members']:
             member = self.clients.web.users.info(user)
             self.clients.web.chat.post_message("@" + member.body['user']['name'], txt, as_user='true')
+            
+   def set_api(self,channel_id):
+        txt = "Please tell me the API URL with `<@" + bot_uid + "> api-http://yoururl`"
+        self.send_message(channel_id, txt)
+   
             
             
         
